edfb22f7cca8ac33742665e7016e2f939d68c4bf
Drew Dimmery <drew.dimmery@gmail.com>
2021-04-19 15:29:38 +0200

    debug proc

--- design_app/processor/dummy.py
+++ design_app/processor/dummy.py
@@ -3,6 +3,9 @@ from .base import Processor
 
 class Dummy(Processor):
     def process(self, cov_dict):
+        print(self.cfg)
+        print(self.var_name)
+        print(cov_dict)
         ohe = [
             float(val == cov_dict[self.var_name])
             for val in self.cfg['values']
